2ebefce66e70203511b6f11c72e3fa10359e36a9
KristianAziz00 <Kristian.ka11@hotmail.com>
2024-11-13 14:00:21 +0100

    ändringar i filen

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-const apiKey = '3e97e5d1f9b8b314fbed9a5d82d0ff3b'
+const apiNyckel = 'b145baa6f3f75a96d9e6d88ac27a7d08';
 
-const url =
-    https;//api.openweathermap.org/data/2.5/weather?id=${cityId}&units=metric&appid=${apiKey};
-    try {     
-    
-    }
 
-catch{
-    
+function hamtaVader(stadId, elementId, stadNamn) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${stadId}&units=metric&appid=${apiNyckel}&lang=sv`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            
+            let temperatur = Math.round(data.main.temp) + "°C";
+            let temperaturF = Math.round((data.main.temp * 9/5) + 32) + "°F";
+            let beskrivning = data.weather[0].description;
+
+            document
+            .getElementById(elementId)
+            .querySelector('h3').innerText = stadNamn;
+
+            document
+                .getElementById(elementId)
+                .querySelector('.temperatur').innerText = `Temperatur: ${temperatur}`;
+
+            document
+                .getElementById(elementId)
+                .querySelector('.beskrivning').innerText = `Beskrivning: ${beskrivning}`;
+        })
+        .catch((error) => {
+            
+            document
+                .getElementById(elementId)
+                .querySelector('.temperatur').innerText = "något blev fel";
+
+            console.error("Fel vid hämtning av väderdata:", error);
+        });
 }
+
+
+document.getElementById('vaderJamforelse').addEventListener('click', () => {
+    
+    const sverigeStadId = document.getElementById('sverigeStad').value;
+    const rwandaStadId = document.getElementById('rwandaStad').value;
+
+    const sverigeStadNamn = document.getElementById('sverigeStad').selectedOptions[0].text;
+    const rwandaStadNamn = document.getElementById('rwandaStad').selectedOptions[0].text;
+    
+    hamtaVader(sverigeStadId, 'sverigeVader', sverigeStadNamn);
+    hamtaVader(rwandaStadId, 'rwandaVader', rwandaStadNamn);
+});

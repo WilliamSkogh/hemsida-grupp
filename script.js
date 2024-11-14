fdc1470d5c71871f634db3aa9949b0e0c9cbf077
HamzeHussein <hamze_hussein@hotmail.com>
2024-11-14 16:29:21 +0100

    gjord ändringar i temaväxlingen frest js och css

---
+++ script.js
@@ -0,0 +1,73 @@
+const apiNyckel = 'b145baa6f3f75a96d9e6d88ac27a7d08';
+
+
+function hamtaVader(stadId, elementId, stadNamn) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?id=${stadId}&units=metric&appid=${apiNyckel}&lang=sv`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            
+            let temperatur = Math.round(data.main.temp) + "°C";
+            let temperaturF = Math.round((data.main.temp * 9/5) + 32) + "°F";
+            let beskrivning = data.weather[0].description;
+
+            document
+            .getElementById(elementId)
+            .querySelector('h3').innerText = stadNamn;
+
+            document
+                .getElementById(elementId)
+                .querySelector('.temperatur').innerText = `Temperatur: ${temperatur}`;
+
+            document
+                .getElementById(elementId)
+                .querySelector('.beskrivning').innerText = `Beskrivning: ${beskrivning}`;
+        })
+        .catch((error) => {
+            
+            document
+                .getElementById(elementId)
+                .querySelector('.temperatur').innerText = "något blev fel";
+
+            console.error("Fel vid hämtning av väderdata:", error);
+        });
+}
+
+
+document.getElementById('vaderJamforelse').addEventListener('click', () => {
+    
+    const sverigeStadId = document.getElementById('sverigeStad').value;
+    const rwandaStadId = document.getElementById('rwandaStad').value;
+
+    const sverigeStadNamn = document.getElementById('sverigeStad').selectedOptions[0].text;
+    const rwandaStadNamn = document.getElementById('rwandaStad').selectedOptions[0].text;
+    
+    hamtaVader(sverigeStadId, 'sverigeVader', sverigeStadNamn);
+    hamtaVader(rwandaStadId, 'rwandaVader', rwandaStadNamn);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+      document.body.classList.add(savedTheme);
+    } else {
+      
+      document.body.classList.add('ljustema');
+      localStorage.setItem('theme', 'ljustema');
+    }
+  });
+  
+  document.getElementById('toggleTheme').addEventListener('click', () => {
+    if (document.body.classList.contains('ljustema')) {
+      document.body.classList.remove('ljustema');
+      document.body.classList.add('morktema');
+      localStorage.setItem('theme', 'morktema');
+    } else {
+      document.body.classList.remove('morktema');
+      document.body.classList.add('ljustema');
+      localStorage.setItem('theme', 'ljustema');
+    }
+  });
+  
